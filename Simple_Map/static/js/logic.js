6ab061b0d6eb0b7d630c08ba1527bdb540145027
ramonmsa <ramonmoraissa@gmail.com>
2021-09-17 21:09:33 -0400

    updating logic.js

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -11,6 +11,18 @@ let map = L.map("mapid", {
   });
 
 
+// L.circle([34.0522, -118.2437], {
+//     radius: 300
+//  }).addTo(map);
+
+ L.circleMarker([34.0522, -118.2437], {
+    radius:300,
+    color: "black",
+    fillColor: '#ffffa1'
+    }).addTo(map);
+
+//  Add a marker to the map for Los Angeles, California.
+let marker = L.marker([34.0522, -118.2437]).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -20,3 +32,5 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 });
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+
